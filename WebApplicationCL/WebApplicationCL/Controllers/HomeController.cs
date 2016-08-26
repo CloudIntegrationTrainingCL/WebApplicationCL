d7f065cf56859690931ee16347a61e280a7484f6
Christian Legg <christian_legg@hotmail.com>
2016-08-26 11:40:00 +0930

    added for loop to hit processing time

--- WebApplicationCL/WebApplicationCL/Controllers/HomeController.cs
+++ WebApplicationCL/WebApplicationCL/Controllers/HomeController.cs
@@ -10,6 +10,10 @@ namespace WebApplicationCL.Controllers
     {
         public ActionResult Index()
         {
+            for (int i = 0; i < 5000; i++)
+            {
+                string test = "1";
+            }
             return View();
         }
 
